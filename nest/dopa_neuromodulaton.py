d3edab5aef703d10b63b31a3b0df93737329b487
Max Talanov <max.talanov@gmail.com>
2015-02-25 17:51:39 +0400

    54edb7821c300a9874f1178d Implement model of 7 compartments

--- nest/dopa_neuromodulaton.py
+++ nest/dopa_neuromodulaton.py
@@ -13,7 +13,8 @@ This is the implementation of experiment of dopamine neuromodulation based on
 https://github.com/research-team/NEUCOGAR/blob/master/experiment_description.md#general-neuromodulation
 and
 test_stdp_dopa
-
+see explanation of neuronal model:
+http://www.nest-initiative.org/Network_burst_generation_by_short-term_plasticity
 It contains:
 
     Cortex
@@ -55,6 +56,9 @@ nu_in = 10.0#2.
 T = 300.0
 dt = 10.0
 
+stdp_dopamine_synapse_weight = 35.
+vt_delay = 1.
+
 # Create spike generators and detectors
 sg_ex = nest.Create('spike_generator', params={'spike_times': ex_spikes_times})
 sg_in = nest.Create('spike_generator', params={'spike_times': in_spikes_times})
@@ -62,7 +66,7 @@ sg_in = nest.Create('spike_generator', params={'spike_times': in_spikes_times})
 sd = nest.Create("spike_detector")
 nest.SetStatus(sd, {"label": "spikes", "withtime": True, "withgid": True, "to_file": True})
 
-#Creating brain
+# Creating brain
 cortex = nest.Create(cortex_neurons_model, cortex_number_of_neurons)
 striatum = nest.Create(striatum_neurons_model, striatum_number_of_neurons)
 gpe = nest.Create(gpe_neurons_model, gpe_number_of_neurons)
@@ -70,6 +74,11 @@ gpi = nest.Create(gpi_neurons_model, gpi_number_of_neurons)
 stn = nest.Create(stn_neurons_model, stn_number_of_neurons)
 snr = nest.Create(snr_neurons_model, snr_number_of_neurons)
 snc = nest.Create(snc_neurons_model, snc_number_of_neurons)
+
+# Volume transmission
+vt = nest.Create("volume_transmitter")
+nest.CopyModel("stdp_dopamine_synapse", "dopa", {"vt": vt[0], "weight": stdp_dopamine_synapse_weight, "delay": vt_delay})
+
 
 
 
