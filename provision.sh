18170337d397a6dc00114e7ecf3685333f96a16e
Alexander Tchitchigin <sad.ronin@gmail.com>
2015-07-08 14:09:12 +0300

    Correct path to download NEST sources. Installing python-tk.

--- provision.sh
+++ provision.sh
@@ -6,10 +6,10 @@ sudo apt-get -y upgrade
 sudo apt-get -y install screen
 
 # NEST dependencies
-sudo apt-get -y install libtool libltdl-dev libgsl0-dev python-all-dev
+sudo apt-get -y install libtool libltdl-dev libgsl0-dev python-all-dev python-tk
 
 # Downloading and installing NEST from sources
-wget http://www.nest-simulator.org/download/gplreleases/nest-2.6.0.tar.gz
+wget http://www.nest-simulator.org/downloads/gplreleases/nest-2.6.0.tar.gz
 cd /tmp
 tar xzf ~/nest-2.6.0.tar.gz
 cd nest-2.6.0
