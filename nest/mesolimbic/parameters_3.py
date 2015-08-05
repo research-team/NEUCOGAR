c1b2cc5e6163402363a0bcf66b36a7bda3bb9126
Max Talanov <max.talanov@gmail.com>
2015-08-05 15:02:18 +0400

    started mesolimbic

---
+++ nest/mesolimbic/parameters_3.py
@@ -0,0 +1,279 @@
+# -*- coding: utf-8 -*-
+
+'''
+It contains:
+
+    Motor Cortex                           =  neurons, iaf_psc_exp glutamatergic
+    Striatum                         =  neurons, iaf_psc_exp GABAergic
+    GPe: globus pallidus external    =  neurons, iaf_psc_exp GABAergic
+    GPi: globus pallidus internal    =  neurons, iaf_psc_exp GABAergic
+    STN: subthalamic nucleus         =  neurons, iaf_psc_exp glutamatergic
+    SNr: substantia nigra reticulata =  neurons, iaf_psc_exp GABAergic
+    SNc: substantia nigra compacta   =  neurons, iaf_psc_exp dopaminergic
+    Thalamus                         =  neurons, iaf_psc_exp glutamatergic
+
+Prefix description:
+    MSN_ - Medium Spiny Neurons
+    ex_  -  excitory
+    inh_ - inhibitory
+    STP_ - Short Term Plasticity
+'''
+# Configure logger
+import logging
+from property_3 import *
+import os
+from matplotlib import collections
+from matplotlib.colors import colorConverter
+import pylab as pl
+
+FORMAT = '%(name)s.%(levelname)s: %(message)s.'
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+
+# general settings
+T = 1000.
+dt = 10.
+
+# neurons number for spike detector
+N_rec = 100
+
+k_ids = 'ids'
+k_name = 'name'
+# functions just for easy settings readability
+'''
+Set up parameters of brain parts (GPe, GPi...).
+If you need new brain part just write name = {k_name: 'name'} and add to iter_all_part variable (dict{'name':neurons_list}).
+'''
+def generate_neurons_BG(nest):
+    logger = logging.getLogger("parameters")
+    iaf_neuronparams = {'E_L': -70., 'V_th': -50., 'V_reset': -67., 'C_m': 2., 't_ref': 2., 'V_m': -60.,
+                        'tau_syn_ex': 1.,
+                        'tau_syn_in': 1.33}
+    # neuron set
+    # k - prefix means key
+    k_NN = 'NN'
+    k_model = 'model'
+    k_coef = 'coefficient'
+    # ===================
+    # BASAL GANGLIA PARTS
+    # ===================
+    motor_cortex = ({k_name: 'motivation'}, {k_name: 'action'})
+    striatum = ({k_name: 'D1'}, {k_name: 'D2'}, {k_name: 'tan'})
+    gpe = {k_name: 'gpe'}
+    gpi = {k_name: 'gpi'}
+    stn = {k_name: 'stn'}
+    snr = {k_name: 'snr'}
+    thalamus = {k_name: 'thalamus'}
+    iter_BG_parts_no_dopa = (gpe, gpi, stn, snr, thalamus) + striatum + motor_cortex
+    snc = {k_name: 'snc'}
+    # ========
+    # NEURONS
+    # ========
+    for bg_part in iter_BG_parts_no_dopa: bg_part[k_model] = 'iaf_psc_exp'
+    # with dopamine
+    snc[k_model] = 'iaf_psc_alpha'
+    iter_all_parts = iter_BG_parts_no_dopa + (snc,)
+    # Count of neurons in every parts of BGs
+    # table for neuron number: https://docs.google.com/spreadsheets/d/1cAm5uosBoKaaPyC1mvb527nYkVcW2YsAA2ECiCDASzg/edit?usp=sharing
+    if test_flag:
+        # ===========
+        # TEST NUMBER
+        # ===========
+        motor_cortex[motivation][k_NN] = 60
+        motor_cortex[action][k_NN] = 150
+        striatum[D1][k_NN] = 30
+        striatum[D2][k_NN] = 30
+        striatum[tan][k_NN] = 8
+        gpe[k_NN] = 30
+        gpi[k_NN] = 10
+        stn[k_NN] = 15
+        snc[k_NN] = 10
+        snr[k_NN] = 21
+        thalamus[k_NN] = 90
+
+        # increase numbers by k
+        k = 5
+        motor_cortex[motivation][k_NN] *= k
+        motor_cortex[action][k_NN] *= k
+        striatum[D1][k_NN] *= k
+        striatum[D2][k_NN] *= k
+        striatum[tan][k_NN] *= k
+        gpe[k_NN] *= k
+        gpi[k_NN] *= k
+        stn[k_NN] *= k
+        snc[k_NN] *= k
+        snr[k_NN] *= k
+        thalamus[k_NN] *= k
+
+    else:
+        # ===========
+        # REAL NUMBER
+        # ===========
+        striatum_NN = 2500000
+        striatum[D1][k_NN] = int(striatum_NN * 0.425)
+        striatum[D2][k_NN] = int(striatum_NN * 0.425)
+        striatum[tan][k_NN] = int(striatum_NN * 0.05)
+        motor_cortex_NN = 29000000
+        motor_cortex[motivation][k_NN] = int(motor_cortex_NN * 0.2)
+        motor_cortex[action][k_NN] = int(motor_cortex_NN * 0.8)
+        gpe[k_NN] = 84100
+        gpi[k_NN] = 12600
+        stn[k_NN] = 22700
+        snc[k_NN] = 12700
+        snr[k_NN] = 47200
+        thalamus[k_NN] = 5000000
+        # =================
+        # COEFFICIENT COUNT
+        # =================
+        # possible different coefficients
+        # k = 0.25
+        # k = 0.0015 # 54640 neurons
+        k = 0.0015 # 54640 neurons
+        motor_cortex[motivation][k_coef] = k
+        motor_cortex[action][k_coef] = k
+        striatum[D1][k_coef] = k
+        striatum[D2][k_coef] = k
+        striatum[tan][k_coef] = k
+        gpe[k_coef] = k
+        gpi[k_coef] = k
+        stn[k_coef] = k
+        snc[k_coef] = k
+        snr[k_coef] = k
+        thalamus[k_coef] = k
+        for part in iter_all_parts: part[k_NN] = int(part[k_NN] * part[k_coef])
+    logger.debug('Initialised: %d neurons' % sum(item[k_NN] for item in iter_all_parts))
+    # assign neuron params to every part of BG
+    nest.SetDefaults('iaf_psc_exp', iaf_neuronparams)
+    nest.SetDefaults('iaf_psc_alpha', iaf_neuronparams)
+
+    for bg_part in iter_all_parts:
+        bg_part[k_ids] = nest.Create(bg_part[k_model], bg_part[k_NN])
+
+    return iter_all_parts
+
+'''
+help method to get neurons_list from iter_BG{'name':neurons_list}
+'''
+def get_ids(name, iter_BG=None):
+    if iter_BG is not None: get_ids.iter_BG = iter_BG
+    for part_BG in get_ids.iter_BG:
+        if part_BG[k_name] == name:
+            return part_BG[k_ids]
+    raise KeyError
+
+# =========
+# SYNAPSES
+# =========
+# poisson generator
+K = 500
+f_part = 0.5
+K_fast = f_part * K
+K_slow = (1.0 - f_part) * K
+
+# Volume transmission
+stdp_dopamine_synapse_w_ex = 85.
+stdp_dopamine_synapse_w_in = -stdp_dopamine_synapse_w_ex
+
+# generator delay
+pg_delay = 20.
+
+# Glutamate
+w_ex = 45.  # excitatory weights
+# GABA
+k = 1.5
+w_in = k * w_ex  # inhibitory weight
+
+STDP_synapseparams = {
+    'model': 'stdp_synapse',
+    'tau_m': {'distribution': 'uniform', 'low': 15., 'high': 25.},
+    'alpha': {'distribution': 'normal_clipped', 'low': 0.5, 'mu': 5.0, 'sigma': 1.0},
+    'delay': {'distribution': 'uniform', 'low': 0.8, 'high': 2.5},
+    'lambda': 0.5
+}
+STDP_synapseparams_ex = dict({'delay': {'distribution': 'uniform', 'low': 0.7, 'high': 1.3},
+                              'weight': w_ex,
+                              'Wmax': 70.
+                             }, **STDP_synapseparams)
+
+STDP_synapseparams_in = dict({'delay': {'distribution': 'uniform', 'low': 1., 'high': 1.9},
+                              'weight': w_in,
+                              'Wmax': -60.
+                             }, **STDP_synapseparams)
+DOPA_synparams = {"delay": 1.}
+DOPA_synparams_ex = dict({"weight": stdp_dopamine_synapse_w_ex, 'Wmax': 100., 'Wmin': 85.}, **DOPA_synparams)
+DOPA_synparams_in = dict({"weight": stdp_dopamine_synapse_w_in, 'Wmax': -100., 'Wmin': -85.}, **DOPA_synparams)
+
+'''
+===========
+ In SNr neurons according to recent experimental findings [http://www.biomedcentral.com/1471-2202/12/S1/P145#B2]
+ has been found short term facilitation in MSN synapses onto SNr neurons from SNc.
+'''
+# ============
+# CONNECTIONS
+# ============
+conn_dict = {'rule': 'all_to_all', 'multapses': True}
+# conn_dict = {'rule': 'fixed_outdegree', 'outdegree': 100}
+# =========
+# USEFUL FUNCTIONS
+# =========
+'''
+Generates string ful name (subfolders with respect to flags defined in properties) of an image
+'''
+def f_name_gen(name, is_image=False):
+    sub_folder = os.path.join(sd_folder_name, 'noise/' if pg_flag else 'static/')
+    if not os.path.exists(sub_folder): os.makedirs(sub_folder)
+
+    return sub_folder + \
+           (name + '_' if len(name) > 0 else '') + \
+           ('yes' if vt_flag else 'no') + '_dopa_generator_' + \
+           ('noise' if pg_flag else 'static') + \
+           ('.png' if is_image else '_')
+
+'''
+if save_weight_flag if True then do plotting of weight change in a synapse
+'''
+def plot_weights(weights_list, title="Neurons weights progress", y_lim=None):
+    # Plot
+    # Make a list of colors cycling through the rgbcmyk series.
+    colors = [colorConverter.to_rgba(c) for c in ('k', 'r', 'g', 'b', 'c', 'y', 'm')]
+
+    axes = pl.axes()
+    ax4 = axes  # unpack the axes
+
+    ncurves = 1
+    offs = (0.0, 0.0)
+
+    segs = []
+    for i in range(ncurves):
+        curve = weights_list
+        segs.append(curve)
+
+    col = collections.LineCollection(segs, offsets=offs)
+    ax4.add_collection(col, autolim=True)
+    col.set_color(colors)
+    ax4.autoscale_view()
+    ax4.set_title(title)
+    ax4.set_xlabel('Time ms')
+    ax4.set_ylabel('Weight pA')
+    y_lim = 105.
+    if y_lim:
+        ax4.set_ylim(-5, y_lim)
+    pl.savefig(f_name_gen('dopa-weights', is_image=True), format='png')
+    # pl.show()
+
+# =======
+# DEVICES
+# =======
+mm_param = {"to_memory": True, "to_file": False, 'withtime': True, 'interval': 0.1,
+            'record_from': ['V_m'], 'withgid': True}
+detector_param = {"label": "spikes", "withtime": True, "withgid": True, "to_file": False, "to_memory": True,
+                  'scientific': True}
+axis = [0, T, -72, -48]
+
+'''
+==========================|
+Medium spiny neurons (MSN) is GABA inhibitory cell. They have dopamine receptors. Dopamine has a dual action on MSNs.
+It inhibits the (D2-type) MSNs in the indirect pathway and
+excites (D1-type) MSNs in the direct pathway.
+Consequently, when dopamine is lost from the striatum, the indirect pathway becomes overactive
+and the direct pathway becomes underactive.
+'''
---
+++ nest/mesolimbic/property_3.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# keys for motor cortex
+motivation = 0
+action = 1
+# keys for striatum
+D1 = 0
+D2 = 1
+tan = 2  # Tonically active neurons
+
+# Quality of graphics
+dpi_n = 120
+
+syn_excitory = "excitatory"
+syn_inhibitory = "inhibitory"
+
+# dopamine model key
+dopa_model_ex = "dopa_ex"
+dopa_model_in = "dopa_in"
+
+sd_folder_name = "output/"
+sd_filename = "spikes-172-0.gdf"
+
+device_static_synapse = "static_device"
+gen_static_syn = "noise_conn"
+# dopamine modulation flag
+vt_flag = True
+# poisson generator with rate set up flag
+pg_flag = True
+# testing mode or real number neurons
+test_flag = True
+#display graphic
+disp_flag = False
+#save weight change when vt_flag is True (a graphic and dat file of weigh change of one synapse)
+save_weight_flag = False
