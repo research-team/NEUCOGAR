0df16b65a15b9de755adcf26950690676981147c
Panzerwaffe <alexey.panzer@gmail.com>
2017-08-14 15:09:53 +0300

    updated script

--- NEST/misc/neuron_models/scripts/neuronmodels.py
+++ NEST/misc/neuron_models/scripts/neuronmodels.py
@@ -5,7 +5,7 @@ import numpy
 
 nest.ResetKernel()
 
-path = "/Users/komarovvitaliy/GitHub/NEUCOGAR/CalculationNM/Results/"
+path = "../Results"
 dpi_n = 130
 
 sysParams = ['synaptic_elements',
@@ -66,15 +66,12 @@ allModels = ['aeif_cond_alpha',
 
 for model in allModels:
     neuron = nest.Create(model)
-    print "\n", model
+    print("\n", model)
     multimeter = nest.Create('multimeter', params={'withtime': True, 'interval': 0.1, 'record_from': ['V_m']})
     generator_ex = nest.Create("spike_generator", params={"spike_times": numpy.array([20.0, 50.0, 58.0]),
                                                           "spike_weights": numpy.array([400., 500., 500.])})
-    generator_in = nest.Create("spike_generator", params={"spike_times": numpy.array([25.0, 30.0, 55.0]),
-                                                          "spike_weights": numpy.array([-400., -500., -500.])})
     nest.Connect(multimeter, neuron)
     nest.Connect(generator_ex, neuron)
-    nest.Connect(generator_in, neuron)
 
     nest.Simulate(100.)
 
@@ -93,78 +90,3 @@ for model in allModels:
     # nest.voltage_trace.from_device(multimeter)
     # nest.voltage_trace.show()
     nest.ResetKernel()
-
-# Test block
-'''
-neuron = nest.Create('iaf_cond_alpha')
-multimeter = nest.Create('multimeter', params = {'withtime': True, 'interval': 0.1, 'record_from':['V_m', 'g_ex', 'g_in']})
-generator_ex = nest.Create("spike_generator", params = {"spike_times": numpy.array([20.0, 50.0, 58.0]),
-                                                     "spike_weights": numpy.array([400., 500., 500.])})
-generator_in = nest.Create("spike_generator", params={"spike_times": numpy.array([25.0, 30.0, 55.0]),
-                                                   "spike_weights": numpy.array([-400., -500., -500.])})
-nest.Connect(multimeter, neuron)
-nest.Connect(generator_ex, neuron)
-nest.Connect(generator_in, neuron)
-
-nest.Simulate(100.)
-
-events = nest.GetStatus(multimeter)[0]['events']
-t = events['times']
-
-pylab.subplot(211)
-pylab.title('iaf_psc_alpha')
-pylab.plot(t, events['V_m'])
-pylab.ylabel('Membrane potential [mV]')
-pylab.draw()
-#pylab.savefig(path + "iaf_psc_alpha" + ".png", dpi=dpi_n, format='png')
-
-pylab.subplot(212)
-pylab.title('iaf_psc_alpha')
-pylab.plot(t, events['g_ex'], t, events['g_in'])
-pylab.xlabel("time (ms)")
-pylab.ylabel('Synaptic conductance (nS)')
-pylab.legend(("g_exc", "g_inh"))
-pylab.draw()
-
-#nest.voltage_trace.from_device(multimeter)
-nest.voltage_trace.show()
-'''
-
-'''
-neuron = nest.Create('aeif_cond_alpha')
-multimeter = nest.Create('multimeter', params = {'withtime': True, 'interval': 0.1, 'record_from':['V_m', 'g_ex', 'g_in', 'w']})
-generator_ex = nest.Create("spike_generator", params = {"spike_times": numpy.array([20.0, 50.0, 58.0]),
-                                                     "spike_weights": numpy.array([400., 500., 500.])})
-generator_in = nest.Create("spike_generator", params={"spike_times": numpy.array([25.0, 30.0, 55.0]),
-                                                   "spike_weights": numpy.array([-400., -500., -500.])})
-nest.Connect(multimeter, neuron)
-nest.Connect(generator_ex, neuron)
-nest.Connect(generator_in, neuron)
-
-nest.Simulate(100.)
-
-events = nest.GetStatus(multimeter)[0]['events']
-t = events['times']
-
-pylab.subplot(311)
-pylab.title('aeif_cond_alpha')
-pylab.plot(t, events['V_m'])
-pylab.ylabel('Membrane potential [mV]')
-pylab.draw()
-#pylab.savefig(path + "iaf_psc_alpha" + ".png", dpi=dpi_n, format='png')
-
-pylab.subplot(312)
-pylab.plot(t, events['g_ex'], t, events['g_in'])
-pylab.xlabel("time (ms)")
-pylab.ylabel('Synaptic conductance (nS)')
-pylab.legend(("g_exc", "g_inh"))
-pylab.draw()
-
-pylab.subplot(313)
-pylab.plot(t, events['w'])
-pylab.xlabel("time (ms)")
-pylab.ylabel('Spike-adaptation (pA)')
-pylab.draw()
-
-nest.voltage_trace.show()
-'''
